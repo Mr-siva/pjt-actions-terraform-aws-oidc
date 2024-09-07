92b64358f2651f7fb77f81b30ee2b7951941e00a
Mr-siva <119104724+Mr-siva@users.noreply.github.com>
2024-09-08 02:50:48 +0530

    provider is changed

--- provider.tf
+++ provider.tf
@@ -3,8 +3,5 @@ terraform {
     bucket = "tf-backend-mrnobody-001"
     key    = "enviconment/terraform.tfstate"
     region = "us-east-1"
-    assume_role = {
-      role_arn = "arn:aws:iam::211125323766:role/aws-tf-oidc-github-action-role"
-    }
   }
 }
